259e50b7de81e158ab3329a5b8c2389350bce2e3
Alexandre Bourget <alex@bourget.cc>
2010-03-07 14:51:20 -0500

    Renamed ._params to .params everywhere. Lots of documentation fixes. Added `.errors`.
    - `.errors` point to the FieldSet's errors. This can be used to indicate we're re-displaying a form which has errors in it.
    - `.params` should be useable by extended classes
    - deprecation warning

---
+++ pylonsapp/pylonsapp/tests/functional/test_owners.py
@@ -0,0 +1,31 @@
+from pylonsapp.tests import *
+
+class TestOwnersController(TestController):
+
+    def test_index(self):
+        resp = self.app.get(url('owners'))
+        resp.mustcontain('gawel')
+
+        resp = self.app.get(url('formatted_owners', format='json'))
+        resp.mustcontain('{"records": [{"url": "/owners/1", "pk": 1, "value": "gawel"}')
+
+    def test_add(self):
+        resp = self.app.post(url('/owners.json'), {"animals": '1', "name": "gawel"})
+        resp.mustcontain('"gawel"', '/owners/')
+
+    def test_view(self):
+        resp = self.app.get(url('/owners/1'))
+        resp.mustcontain('gawel')
+
+        resp = self.app.get(url('/owners/1.json'))
+        resp.mustcontain('"gawel"')
+
+    def test_edit(self):
+        resp = self.app.get(url('/owners/1/edit'))
+        resp.mustcontain('<form action="/owners/1" method="POST" enctype="multipart/form-data">',
+                         'gawel')
+
+    def test_update(self):
+        resp = self.app.put(url('/owners/31.json'), '{"animals": [1], "name": "gawel"}')
+        resp.mustcontain('"gawel"', '/owners/31')
+
