cdb16c3679f6eb8e1f18bee7259e9f0b8af2249b
ikken <ikken@localhost>
2010-11-22 23:12:00 +0100

    Added Polish translation.

---
+++ pylonsapp/pylonsapp/tests/functional/test_owners.py
@@ -0,0 +1,31 @@
+from pylonsapp.tests import *
+
+class TestOwnersController(TestController):
+
+    def test_index(self):
+        resp = self.app.get(url('owners'))
+        resp.mustcontain('gawel')
+
+        resp = self.app.get(url('formatted_owners', format='json'))
+        resp.mustcontain('"rows": [{"item_url": "/owners')
+
+    def test_add(self):
+        resp = self.app.post(url('/owners.json'), {"Owner--animals": '1', "Owner--name": "gawel"})
+        resp.mustcontain('"gawel"', '/owners/')
+
+    def test_view(self):
+        resp = self.app.get(url('/owners/1'))
+        resp.mustcontain('gawel')
+
+        resp = self.app.get(url('/owners/1.json'))
+        resp.mustcontain('"gawel"')
+
+    def test_edit(self):
+        resp = self.app.get(url('/owners/1/edit'))
+        resp.mustcontain('<form action="/owners/1" method="POST" enctype="multipart/form-data">',
+                         'gawel')
+
+    def test_update(self):
+        resp = self.app.put(url('/owners/31.json'), '{"Owner-31-animals": [1], "Owner-31-name": "gawel"}')
+        resp.mustcontain('"gawel"', '/owners/31')
+
